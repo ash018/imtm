4567bfe227e078bd40695b703219d01ccfaf001b
MIS-IA-Akash <smakash@aci-bd.local>
2018-08-28 22:14:14 +0600

    Before night

---
+++ application/views/login_page.php
@@ -0,0 +1,86 @@
+<?php
+    defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>pms</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="<?php echo base_url()?>assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="<?php echo base_url()?>assets/css/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="<?php echo base_url()?>assets/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="<?php echo base_url()?>assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="login-panel panel panel-default">
+                    <div class="panel-green">
+                        <h3 class="panel-title">Please Sign In</h3>
+                    </div>
+                    <div class="panel-body">
+                    <?php if(isset($login_error) && $login_error != ''){?>
+                        <div class="alert alert-danger">
+                            <?php echo $login_error;?>
+                        </div>
+                    <?php }?>
+                        <form role="form" action="<?php echo base_url(); ?>Admin/user_login_check" method="post">
+                            <fieldset>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="User Name" name="username" type="text" autofocus>
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="Password" name="password" type="password" value="">
+                                </div>
+                                <div class="checkbox">
+                                    <label>
+                                        <input name="remember" type="checkbox" value="Remember Me">Remember Me
+                                    </label>
+                                </div>
+                                <!-- Change this to a button or input when using this as a form -->
+<!--                                <a href="index.html" class="btn btn-lg btn-success btn-block">Login</a>-->
+                                <input type="submit" class="btn btn-lg btn-success btn-block" value="Login"/>
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="<?php echo base_url()?>assets/js/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo base_url()?>assets/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="<?php echo base_url()?>assets/js/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="<?php echo base_url()?>assets/js/sb-admin-2.js"></script>
+
+</body>
+
+</html>
+
